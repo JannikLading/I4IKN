b1da163769310da423aabe6ea8e42c21d300d9a2
root <jannik.lading@hotmail.dk>
2019-03-14 14:59:29 +0100

    modified Client

--- exercise7/exercise7/Client.cs
+++ exercise7/exercise7/Client.cs
@@ -1,10 +1,22 @@
 ﻿using System;
-namespace exercise7
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDP
 {
-    public class Program
+    public class Client
     {
-        public Program()
+		static void Main(string[] args)
         {
+            UDPSocket s = new UDPSocket();
+            s.Server("10.0.0.2", 9000);
+
+            UDPSocket c = new UDPSocket();
+            c.Client("10.0.0.1", 9000);
+            c.Send("TEST!");
+
+            Console.ReadKey();
         }
     }
 }
