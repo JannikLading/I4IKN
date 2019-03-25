01c1da60a2d3228cecee393f1f307de1fd721284
root <alexander8@hotmail.com>
2019-03-25 15:07:11 +0100

    format code

--- exercise7/exercise7alex/UDP/get_measurement/Program.cs
+++ exercise7/exercise7alex/UDP/get_measurement/Program.cs
@@ -5,40 +5,36 @@ using System.Text;
 
 namespace get_measurement
 {
-    class get_measurement
-    {
-        // IKN: prøv at initialisere både receiver udpclient listener og sender socket 
-        // på samme tid. Det er muligt at serveren sendte sit svar til client,
-        // inden client er klar til at modtage svaret, og derved bare bliver 
-        //ved med at vente for ever.
-        private const int port = 9000;
+	class get_measurement
+	{
+		private const int port = 9000;
 
-        public static void Main(string[] args)
-        {
-            // socket to send to
-            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            IPAddress serverIp = IPAddress.Parse(args[0]);
+		public static void Main(string[] args)
+		{
+			// socket to send to
+			Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+			IPAddress serverIp = IPAddress.Parse(args[0]);
 
-            // Socket to receive ready
-            UdpClient udpListener = new UdpClient(port);
-            IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, port);
+			// Socket to receive ready
+			UdpClient udpListener = new UdpClient(port);
+			IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, port);
 
-            // from string to bytes
-            byte[] sendbuf = Encoding.ASCII.GetBytes(args[1]);
-            IPEndPoint ep = new IPEndPoint(serverIp, port);
+			// from string to bytes
+			byte[] sendbuf = Encoding.ASCII.GetBytes(args[1]);
+			IPEndPoint ep = new IPEndPoint(serverIp, port);
 
-            s.SendTo(sendbuf, ep);
+			s.SendTo(sendbuf, ep);
 
-            Console.WriteLine("Message sent to server");
+			Console.WriteLine("Message sent to server");
 
-            //Actually read
-            Console.WriteLine("Before listening");
-            byte[] bytes = udpListener.Receive(ref groupEP);
-            Console.WriteLine("After listening");
-            Console.WriteLine($" {Encoding.ASCII.GetString(bytes, 0, bytes.Length)}");
+			//Actually read
+			Console.WriteLine("Before listening");
+			byte[] bytes = udpListener.Receive(ref groupEP);
+			Console.WriteLine("After listening");
+			Console.WriteLine($" {Encoding.ASCII.GetString(bytes, 0, bytes.Length)}");
 
-            udpListener.Close();
+			udpListener.Close();
 
-        }
-    }
+		}
+	}
 }
--- exercise7/exercise7alex/UDP/measurement_server/Program.cs
+++ exercise7/exercise7alex/UDP/measurement_server/Program.cs
@@ -9,82 +9,83 @@ using System.Net.Sockets;
 
 namespace UDP
 {
-    public class measurement_server
-    {
-        private const int port = 9000;
+	public class measurement_server
+	{
+		private const int port = 9000;
 
-        private static void StartListener()
-        {
+		private static void StartListener()
+		{
 
-           
-            while (true)
-            {
+
+			while (true)
+			{
 				UdpClient udpListener = new UdpClient(port);
 
-                // Needed to read datagram
-                IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, port);
+				// Needed to read datagram
+				IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, port);
 
-                Console.WriteLine("Waiting for clients");
-                byte[] bytes = udpListener.Receive(ref groupEP);
+				Console.WriteLine("Waiting for clients");
+				byte[] bytes = udpListener.Receive(ref groupEP);
 
-                Console.WriteLine($"Received request from {groupEP} :");
-                Console.WriteLine($" {Encoding.ASCII.GetString(bytes, 0, bytes.Length)}");
+				Console.WriteLine($"Received request from {groupEP} :");
+				Console.WriteLine($" {Encoding.ASCII.GetString(bytes, 0, bytes.Length)}");
 
 				string clientMsg = Encoding.ASCII.GetString(bytes);
 
 				if (clientMsg.ToString() == "U" || clientMsg.ToString() == "u")
 				{
 					var buffer = File.ReadAllText("/proc/uptime");
-                    
+
 					Console.WriteLine($" Message to send: {buffer}");
 					Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-                    
-                    IPAddress clientIp = IPAddress.Parse("10.0.0.2");
 
-                    IPEndPoint ep = new IPEndPoint(clientIp, port);
+					IPAddress clientIp = IPAddress.Parse("10.0.0.2");
+
+					IPEndPoint ep = new IPEndPoint(clientIp, port);
 
 					byte[] buf = Encoding.ASCII.GetBytes($"{buffer}");
 
 					s.SendTo(buf, ep);
-					               
-                    Console.WriteLine("Message sent to client");
+
+					Console.WriteLine("Message sent to client");
 
 					s.Close();
-     
-                }
-                else if (clientMsg.ToString() == "l" || clientMsg.ToString() == "L")
-                {
+
+				}
+				else if (clientMsg.ToString() == "l" || clientMsg.ToString() == "L")
+				{
 					var buffer = File.ReadAllText("/proc/loadavg");
 
-                    Console.WriteLine($" Message to send: {buffer}");
-                    // socket to send to
-                    Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+					Console.WriteLine($" Message to send: {buffer}");
 
-                    IPAddress clientIp = IPAddress.Parse("10.0.0.2");
+					// socket to send to
+					Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 
-                    IPEndPoint ep = new IPEndPoint(clientIp, port);
+					IPAddress clientIp = IPAddress.Parse("10.0.0.2");
 
-                    // from string to bytes
-                    byte[] buf = Encoding.ASCII.GetBytes($"{buffer}");
+					IPEndPoint ep = new IPEndPoint(clientIp, port);
 
-                    s.SendTo(buf, ep);
+					// from string to bytes
+					byte[] buf = Encoding.ASCII.GetBytes($"{buffer}");
 
-                    Console.WriteLine("Message sent to client");
+					s.SendTo(buf, ep);
+
+					Console.WriteLine("Message sent to client");
 
 					s.Close();
-                }
+				}
 				else
-                {
-                    Console.WriteLine("Illegal request from client");
-                }
+				{
+					Console.WriteLine("Illegal request from client");
+				}
 
 				udpListener.Close();
-            }
-           }
+			}
+		}
 
-        public static void Main()
-        {
-            StartListener();
-        }
-    }
+		public static void Main()
+		{
+			StartListener();
+		}
+	}
 }
